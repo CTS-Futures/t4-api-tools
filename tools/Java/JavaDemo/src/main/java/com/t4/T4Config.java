4f692eb7f5f05fff0b3be0781dd040df7bdb0119
Janiya Garner <janiya.garner@plus500.com>
2025-07-02 13:21:35 -0500

    Connection pane UI, using config file, bug fixes and code cleaning.

---
+++ tools/Java/JavaDemo/src/main/java/com/t4/T4Config.java
@@ -0,0 +1,12 @@
+package com.t4;
+
+public class T4Config {
+    public static final String WS_URL = "wss://wss-sim.t4login.com/v1";
+    public static final String API_KEY = "";
+    public static final String FIRM = "CTS";
+    public static final String USERNAME = "JGarner";
+    public static final String PASSWORD = "Temp123$";
+    public static final String APP_NAME = "T4WebSite";
+    public static final String APP_LICENSE = "81CE8199-0D41-498C-8A0B-EC5510A395F4";
+}
+
