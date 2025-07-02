233c21608c8ad56eced7098b27514f9c68c6979e
Janiya Garner <janiya.garner@plus500.com>
2025-07-02 15:28:59 -0500

    Started on Market pane

--- tools/Java/JavaDemo/src/main/java/com/t4/MarketDataPane.java
+++ tools/Java/JavaDemo/src/main/java/com/t4/MarketDataPane.java
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 /* package com.t4;
 
 import javafx.application.Platform;
@@ -110,10 +111,13 @@ public class MarketDataPane extends VBox {
 } */
 
 
+=======
+>>>>>>> e7263a4 (Started on Market pane)
 package com.t4;
 
 import javafx.application.Platform;
 import javafx.geometry.Insets;
+<<<<<<< HEAD
 import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.layout.*;
@@ -241,3 +245,54 @@ public class MarketDataPane extends VBox {
         Platform.runLater(() -> selectMarketButton.setDisable(!enable));
     }
 }
+=======
+import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
+
+
+public class MarketDataPane extends VBox{
+    private final Label symbolLabel = new Label("Symbol: --");
+    private final Label bidLabel = new Label("Bid: --");
+    private final Label askLabel = new Label("Ask: --");
+    private final Label lastLabel = new Label("Last: --");
+
+    public MarketDataPane(){
+        Label titleLabel = new Label("Market Data");
+        titleLabel.setFont(new Font("Arial", 18));
+
+        GridPane grid = new GridPane();
+        grid.setVgap(10);
+        grid.setHgap(20);
+        grid.setPadding(new Insets(10));
+
+        grid.add(symbolLabel, 0, 0);
+        grid.add(bidLabel, 0, 1);
+        grid.add(askLabel, 1, 1);
+        grid.add(lastLabel, 0, 2);
+
+        this.setSpacing(10);
+        this.setPadding(new Insets(15));
+        this.getChildren().addAll(titleLabel, grid);
+        this.setStyle("-fx-border-color: lightgray; -fx-border-radius: 5; -fx-background-color: #fdfdfd;");
+    }
+
+    public void updateSymbol(String symbol) {
+        Platform.runLater(() -> symbolLabel.setText("Symbol: " + symbol));
+    }
+
+    public void updateBid(String bid) {
+        Platform.runLater(() -> bidLabel.setText("Bid: " + bid));
+    }
+
+    public void updateAsk(String ask) {
+        Platform.runLater(() -> askLabel.setText("Ask: " + ask));
+    }
+
+    public void updateLast(String last) {
+        Platform.runLater(() -> lastLabel.setText("Last: " + last));
+    }
+    
+}
+>>>>>>> e7263a4 (Started on Market pane)
