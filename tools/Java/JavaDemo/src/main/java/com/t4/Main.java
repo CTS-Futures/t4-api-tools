e165e43e9e42b498612b76cc53e39e3be7f58953
Janiya Garner <janiya.garner@plus500.com>
2025-07-03 15:03:47 -0500

    Started the market pane

--- tools/Java/JavaDemo/src/main/java/com/t4/Main.java
+++ tools/Java/JavaDemo/src/main/java/com/t4/Main.java
@@ -2,15 +2,21 @@ package com.t4;
 import com.t4.ConnectionUI;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import com.t4.MarketDataPane;
 
 public class Main extends Application{
     @Override
     public void start(Stage primaryStage) {
         T4APIClientTest client = T4APIClientTest.getInstance();
         ConnectionUI connectionPane = new ConnectionUI(client);
+        MarketDataPane marketPane = new MarketDataPane();
 
-        Scene scene = new Scene(connectionPane, 500, 200);
+        T4APIClientTest.getInstance().setMarketDataP(marketPane);
+        //Scene scene = new Scene(connectionPane, 500, 200);
+        VBox root = new VBox(connectionPane, marketPane);
+        Scene scene = new Scene(root, 500, 400);
         primaryStage.setTitle("T4 API Client - Connection");
         primaryStage.setScene(scene);
         primaryStage.show();
