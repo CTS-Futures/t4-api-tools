5eda47152dc4a89135c194ba9d12c775f9fa0851
Chad <chad.lowe@plus500.com>
2025-09-11 19:36:21 -0500

    Added project to demonstrate pulling chart data in binary format.

---
+++ tools/dotNet/T4BinaryChartDataDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using T4BinaryChartDataDemo;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureAppConfiguration((context, config) =>
+    {
+        config.SetBasePath(Directory.GetCurrentDirectory())
+              .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+              .AddJsonFile($"appsettings.{context.HostingEnvironment.EnvironmentName}.json", optional: true)
+              .AddUserSecrets<Program>(optional: true);
+    })
+    .ConfigureServices((context, services) =>
+    {
+        services.Configure<T4DemoConfig>(context.Configuration.GetSection("T4Demo"));
+        services.AddTransient<Demo>();
+
+    })
+    .Build();
+
+
+
+var demo = host.Services.GetRequiredService<Demo>();
+await demo.Run();
